a4cdcfa523532ae12b1ff119f0ef85f54fe80a21
Meakelvis <markelvis37@gmail.com>
2019-06-30 10:03:20 +0300

    added test_msisdn

---
+++ keys.py
@@ -0,0 +1,7 @@
+business_shortCode = "174379"  # lipa na mpesa short code
+phone_number = "254740130441"
+lipa_na_mpesa_passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
+consumer_key = "tO0rRBTNcT3Ul5BMbQLgF7P3TAgcBmaM"
+consumer_secret = "vjtmmsqj3TLYKFGU"
+shortcode = 601451
+test_msisdn = 254708374149
